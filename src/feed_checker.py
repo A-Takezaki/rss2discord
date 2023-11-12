2dcf6c45b3e22a872298f613acabd9c326d4511b
A-Takezaki <zaki.fmworks@gmail.com>
2023-11-13 01:18:53 +0900

    コメントをリファクタリング

--- src/feed_checker.py
+++ src/feed_checker.py
@@ -4,32 +4,36 @@ import requests
 import sqlite3
 import os
 
-# 環境変数から設定を読み込むか、デフォルトのパスを使用します。
+# CONFIGファイルのパスを環境変数から取得、なければデフォルトのパスを使用
 CONFIG_PATH = os.getenv('CONFIG_PATH', '../config.ini')
 
 def load_config(path):
+    # RawConfigParserを使用してCONFIGファイルを読み込み、設定値を取得
     import configparser
     config = configparser.RawConfigParser()
     config.read(path)
     return config['DEFAULT']['DiscordWebhookUrl'], config['DEFAULT']['RssFeedUrl']
 
-# 設定を読み込みます。
+# CONFIGファイルからDiscordのWebhook URLとRSSフィードURLを読み込む
 WEBHOOK_URL, RSS_FEED_URL = load_config(CONFIG_PATH)
-# Get the directory where the script is located
+
+# スクリプトが存在するディレクトリの絶対パスを取得
 script_dir = os.path.dirname(os.path.realpath(__file__))
-# Construct the path to the database file
+# データベースファイルへの絶対パスを構築
 DATABASE_PATH = os.path.join(script_dir, '../db/posted_entries.db')
+
 def post_to_discord(entry):
-    # Discordに投稿するためのデータを作成します。
+    # Discordへの投稿内容を準備
     data = {
         "content": f"{entry.title}\n{entry.link}"
     }
-    # DiscordのWebhook URLにPOSTリクエストを送信します。
+    # DiscordのWebhook URLへ投稿
     response = requests.post(WEBHOOK_URL, json=data)
-    return response.status_code == 204  # 204は成功のステータスコードです。
+    # ステータスコード204が返れば成功
+    return response.status_code == 204
 
 def entry_already_posted(entry_id):
-    # データベースを確認してエントリが既に投稿されたかどうかをチェックします。
+    # 指定されたIDのエントリがデータベースに存在するか確認
     conn = sqlite3.connect(DATABASE_PATH)
     c = conn.cursor()
     c.execute("SELECT id FROM posted_entries WHERE id = ?", (entry_id,))
@@ -38,28 +42,35 @@ def entry_already_posted(entry_id):
     return exists
 
 def mark_entry_as_posted(entry_id, entry):
-    # エントリをデータベースに記録します。
+    # エントリをデータベースに記録（summaryは現在使用しないためNoneを挿入）
     conn = sqlite3.connect(DATABASE_PATH)
     c = conn.cursor()
-    c.execute("INSERT INTO posted_entries (id, title, summary, url) VALUES (?, ?, ?, ?)",
-              (entry_id, entry.title, None, entry.link))
+    c.execute("INSERT INTO posted_entries (id, title, url) VALUES (?, ?, ?)",
+              (entry_id, entry.title, entry.link))
     conn.commit()
     conn.close()
 
 def check_feed_and_post_entries():
-    # RSSフィードを解析します。
+    # RSSフィードを解析してエントリを処理
     feed = feedparser.parse(RSS_FEED_URL)
+    # フィードのタイトルとエントリ数を出力
     print(f"Feed Title: {feed.feed.get('title', 'No feed title')}")
     print(f"Feed Entries: {len(feed.entries)}")
 
+    # 取得したエントリごとに処理
     for entry in feed.entries:
-        entry_id = entry.get('id', entry.link)  # idがなければ、linkをエントリIDとして使用します。
-        if not entry_already_posted(entry_id):  # エントリがまだ投稿されていない場合
-            if post_to_discord(entry):  # Discordに投稿を試みます。
-                mark_entry_as_posted(entry_id, entry)  # 成功したら、データベースに記録します。
+        entry_id = entry.get('id', entry.link)  # idがなければlinkをidとして使用
+        # 未投稿のエントリのみを処理
+        if not entry_already_posted(entry_id):
+            # Discordへの投稿を試みる
+            if post_to_discord(entry):
+                # 投稿成功したらデータベースに記録
+                mark_entry_as_posted(entry_id, entry)
                 print(f"Posted to Discord: {entry.title}")
             else:
+                # 投稿失敗を出力
                 print(f"Failed to post to Discord: {entry.title}")
 
+# スクリプトのメイン処理を実行
 if __name__ == '__main__':
     check_feed_and_post_entries()
